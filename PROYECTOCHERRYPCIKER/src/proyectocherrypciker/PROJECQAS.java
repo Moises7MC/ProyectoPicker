57f162b92738485d904859a2b3ba2a07dc5594c9
Practicante - Llalle <pracsupsiscc01@azucarperu.com.pe>
2023-06-22 10:18:42 -0500

    Comiteando QAS 2

---
+++ PROYECTOCHERRYPCIKER/src/proyectocherrypciker/PROJECQAS.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocherrypciker;
+
+/**
+ *
+ * @author pracsupsiscc01
+ */
+public class PROJECQAS {
+    public static void main(String[] args) {
+        System.out.println("MANUALAZO QAS");
+    }
+}
